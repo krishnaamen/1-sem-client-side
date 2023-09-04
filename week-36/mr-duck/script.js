aed6b748f66e25956af98ad799caca846a2a05d7
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-09-04 11:53:40 +0200

    More code from 4th of september class, added fetch for joke

--- week-36/mr-duck/script.js
+++ week-36/mr-duck/script.js
@@ -2,6 +2,7 @@ const sendButton = document.getElementById("send-button");
 const inputField = document.querySelector("#input-field");
 const messagesContainer = document.querySelector(".messages");
 const nameInput = document.querySelector("#my-name");
+const forceButton = document.querySelector("#force");
 
 function handleNameInput() {
   console.log("Hello from name input function");
@@ -55,3 +56,44 @@ function newDateInRequestedFormat() {
 
   return toReturn;
 }
+
+function bruteForcingDateFormat() {
+  const now = new Date();
+
+  const date = now.getDate();
+  const month = now.getMonth();
+  const year = now.getFullYear();
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+
+  const datePart = `${date}/${month}/${year}`;
+  const timePart = `${hour}:${minutes}`;
+
+  return datePart + ", " + timePart;
+}
+
+async function handleForce() {
+  // 1 todo: Get the joke
+  const response = await fetch("https://v2.jokeapi.dev/joke/Any", {
+    method: "GET",
+  });
+
+  const body = await response.json();
+
+  console.log(body);
+
+  const type = body.type;
+
+  if (type == "twopart") {
+    const setup = body.setup;
+    const delivery = body.delivery;
+  } else {
+    const joke = body.joke;
+  }
+
+  // 2 todo: Create the new P element
+
+  // 3 todo: Push that new P element to the DOM.
+}
+
+forceButton.addEventListener("click", handleForce);
